f80f5d746a6b3333cd580bbadf8076601940e6b3
jblagana <79191351+jblagana@users.noreply.github.com>
2023-08-20 01:06:15 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function openMe(){
+	x = document.getElementById("test");
+	// x.style.display = "block";
+
+	x.className = "open";
+}
+
+function closeMe(){
+	x = document.getElementById("test");
+	// x.style.display = "none";
+
+	x.className = "close";
+}
---
+++ js/main.js
@@ -0,0 +1,11 @@
+function msg(msg){
+	document.getElementById('output').innerHTML += msg + " event<br>";
+}
+
+function setDate(){
+	document.getElementById('date').innerHTML = Date();
+}
+
+function hey() {
+	alert("Tonton's tummy is big!")
+}
